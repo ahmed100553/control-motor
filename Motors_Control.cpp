c940b5bf5abc201ed0232672f4f19cce40bdcb2b
AhmedAbdulla100553 <zekokaka56@yahoo.com>
2016-02-12 22:16:06 +0200

    Create Motors_Control.cpp

---
+++ Motors_Control.cpp
@@ -0,0 +1,53 @@
+#include "Arduino.h"
+#include "Motors_Control.h"
+
+Motors_Control::Motors_Control(int pin, int pin2, int pin3, int pin4)
+{
+    pinMode(pin, OUTPUT);
+    pinMode(pin2, OUTPUT);
+    pinMode(pin3, OUTPUT);
+    pinMode(pin4, OUTPUT);
+    _pin = pin;
+    _pin2 = pin2;
+    _pin3 = pin3;
+    _pin4 = pin4;    
+}
+void Motors_Control::FORWARD(int speed)
+{
+    _speed = speed;
+    analogWrite(_pin, 0);
+    analogWrite(_pin2, _speed);
+    analogWrite(_pin3, _speed);
+    analogWrite(_pin4, 0);
+}
+void Motors_Control::BACKWARD(int speed)
+{
+    _speed = speed;
+    analogWrite(_pin, speed);
+    analogWrite(_pin2, 0);
+    analogWrite(_pin3, 0);
+    analogWrite(_pin4, speed);
+}
+void Motors_Control::STOP()
+{
+    analogWrite(_pin, 0);
+    analogWrite(_pin2, 0);
+    analogWrite(_pin3, 0);
+    analogWrite(_pin4, 0);
+}
+void Motors_Control::LEFT(int speed)
+{
+    _speed = speed;
+    analogWrite(_pin, 0);
+    analogWrite(_pin2, speed);
+    analogWrite(_pin3, 0);
+    analogWrite(_pin4, speed);
+}
+void Motors_Control::RIGHT(int speed)
+{
+    _speed = speed;
+    analogWrite(_pin, speed);
+    analogWrite(_pin2, 0);
+    analogWrite(_pin3, speed);
+    analogWrite(_pin4, 0);
+}
