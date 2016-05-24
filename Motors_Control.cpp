fd68b3b7c3197a76be619d5e837072b86ec7187d
Ahmed Abdulla <ahmedsendel@gmail.com>
2016-05-24 23:02:23 +0200

    Update Motors_Control.cpp

--- Motors_Control.cpp
+++ Motors_Control.cpp
@@ -7,47 +7,40 @@ Motors_Control::Motors_Control(int pin, int pin2, int pin3, int pin4)
     pinMode(pin2, OUTPUT);
     pinMode(pin3, OUTPUT);
     pinMode(pin4, OUTPUT);
-    _pin = pin;
-    _pin2 = pin2;
-    _pin3 = pin3;
-    _pin4 = pin4;    
 }
 void Motors_Control::FORWARD(int speed)
 {
-    _speed = speed;
-    analogWrite(_pin, 0);
-    analogWrite(_pin2, _speed);
-    analogWrite(_pin3, _speed);
-    analogWrite(_pin4, 0);
+    analogWrite(pin, 0);
+    analogWrite(pin2, speed);
+    analogWrite(pin3, speed);
+    analogWrite(pin4, 0);
 }
 void Motors_Control::BACKWARD(int speed)
 {
-    _speed = speed;
-    analogWrite(_pin, _speed);
-    analogWrite(_pin2, 0);
-    analogWrite(_pin3, 0);
-    analogWrite(_pin4, _speed);
+	
+    analogWrite(pin, speed);
+    analogWrite(pin2, 0);
+    analogWrite(pin3, 0);
+    analogWrite(pin4, speed);
 }
 void Motors_Control::STOP()
 {
-    analogWrite(_pin, 0);
-    analogWrite(_pin2, 0);
-    analogWrite(_pin3, 0);
-    analogWrite(_pin4, 0);
+    analogWrite(pin, 0);
+    analogWrite(pin2, 0);
+    analogWrite(pin3, 0);
+    analogWrite(pin4, 0);
 }
 void Motors_Control::LEFT(int speed)
 {
-    _speed = speed;
-    analogWrite(_pin, 0);
-    analogWrite(_pin2, _speed);
-    analogWrite(_pin3, 0);
-    analogWrite(_pin4, _speed);
+    analogWrite(pin, 0);
+    analogWrite(pin2, speed);
+    analogWrite(pin3, 0);
+    analogWrite(pin4, speed);
 }
 void Motors_Control::RIGHT(int speed)
 {
-    _speed = speed;
-    analogWrite(_pin, _speed);
-    analogWrite(_pin2, 0);
-    analogWrite(_pin3, _speed);
-    analogWrite(_pin4, 0);
+    analogWrite(pin, speed);
+    analogWrite(pin2, 0);
+    analogWrite(pin3, speed);
+    analogWrite(pin4, 0);
 }
